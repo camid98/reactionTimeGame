1f1d609c57f353a5bd4efd0f2536214ebb937aa5
camid98 <cdodson@cdodson901.local>
2015-09-08 19:56:37 -0700

    Colors.h Header
    Defining the RGB colors I want to use in the program.
    
    Will later add color-pattern definitions here.
    
    To-Do: change green to be even more green (it’s too aqua at this point)

---
+++ Colors.h
@@ -0,0 +1,28 @@
+// See file in DropBox Personal > Art-Tech Projects > Tech Gauntlet Game > "concepts.sketch" for all the colors here
+
+#define SALMON       0
+#define PINK         1
+#define PURPLE       2
+#define BLUE         3
+#define SKY_BLUE     4
+#define LIGHT_BLUE   5
+#define GREEN        6
+#define YELLOW       7
+#define ORANGE       8
+#define ORANGE_RED   9
+
+static const int NUM_COLORS = 10;
+
+static const int COLORS[NUM_COLORS][3] = {
+{172, 16, 16}, // SALMON
+{201, 13, 121},// PINK
+{78, 18, 178},// PURPLE
+{0, 76, 209},// BLUE
+{10, 130, 178},// SKY_BLUE
+{10, 172, 184},// LIGHT_BLUE
+{15, 186, 59},// GREEN
+{185, 190, 16},// YELLOW
+{182, 135, 0},// ORANGE
+{192, 88, 5},// ORANGE_RED
+};
+
