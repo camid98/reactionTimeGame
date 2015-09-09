f41f3bc25cf1828ad2a9c3126e45c3f3f78b225b
camid98 <cdodson@cdodson901.local>
2015-09-08 19:59:50 -0700

    Setting new Pin values
    Had to change the RGB pins to non-A# pins, as analog pins didn’t output
    analog.

--- lilypadPins.h
+++ lilypadPins.h
@@ -1,13 +1,13 @@
 #define MOTOR 3
 #define RGB_LED_RED 11
-#define RGB_LED_BLUE A3
-#define RGB_LED_GREEN A2
+#define RGB_LED_GREEN 10
+#define RGB_LED_BLUE 9
 #define BUTTON A5
 //#define SLIDE_SWITCH 2
 
 #define WHITE_1 6
-#define WHITE_2 9
-#define WHITE_3 10
+#define WHITE_2 A3
+#define WHITE_3 A2
 #define WHITE_4 5
 #define WHITE_5 A4
 
@@ -15,3 +15,4 @@
 
 #define TEMPERATURE_PIN A1
 #define LIGHT_SENSOR A6
+
