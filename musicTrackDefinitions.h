dbbb6727a6c89a90133c7b1b5dbb6cca0cd41179
camid98 <cdodson@cdodson901.local>
2015-09-08 20:00:57 -0700

    Music Definitions
    Win, Lose, mini-win & mini-lose songs

---
+++ musicTrackDefinitions.h
@@ -0,0 +1,62 @@
+// Define your music here
+
+// Star Wars Theme Song
+#define STAR_WARS_NUM 19
+unsigned int starWars[STAR_WARS_NUM][3] ={
+{NOTE_C4, 100, 50},
+{NOTE_C4, 100, 50},
+{NOTE_C4, 100, 50},
+{NOTE_C4, 400, 50},
+
+{NOTE_G4, 400, 50},
+{NOTE_F4, 100, 50},
+{NOTE_E4, 100, 50},
+{NOTE_D4, 100, 50},
+
+{NOTE_C5, 400, 50},
+{NOTE_G4, 400, 50},
+{NOTE_F4, 100, 50},
+{NOTE_E4, 100, 50},
+{NOTE_D4, 100, 50},
+
+{NOTE_C5, 400, 50},
+{NOTE_G4, 400, 50},
+{NOTE_F4, 100, 50},
+{NOTE_E4, 100, 50},
+{NOTE_F4, 100, 50},
+{NOTE_D4, 400, 50}
+};
+
+#define MINI_WIN_NUM 2
+unsigned int miniWinMusic[MINI_WIN_NUM][3] = {
+{NOTE_C5, 400, 50},
+{NOTE_C5, 400, 50}
+};
+
+#define MINI_LOSE_NUM 2
+unsigned int miniLoseMusic[MINI_LOSE_NUM][3] = {
+{NOTE_D4, 400, 50},
+{NOTE_D4, 400, 50}
+};
+
+#define LOSE_MUSIC_NUM 16
+unsigned int loseMusic[LOSE_MUSIC_NUM][3] = {
+{NOTE_C3, 100, 50},
+{NOTE_A2, 100, 50},
+{NOTE_B2, 100, 50},
+{NOTE_G2, 100, 50},
+{NOTE_A2, 100, 50},
+{NOTE_F2, 100, 50},
+{NOTE_G2, 100, 50},
+
+{NOTE_C3, 100, 50},
+{NOTE_A2, 100, 50},
+{NOTE_B2, 100, 50},
+{NOTE_G2, 100, 50},
+{NOTE_A2, 100, 50},
+{NOTE_F2, 100, 50},
+{NOTE_G2, 100, 50},
+
+{NOTE_D2, 400, 50},
+{NOTE_C2, 400, 50}
+};
